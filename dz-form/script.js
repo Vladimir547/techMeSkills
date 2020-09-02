e07b9b75b19df7386300149a9e60659c36e86774
Vladimir547 <germes547@gmail.ru>
2020-09-02 16:09:47 +0300

    fix remarks

--- dz-form/script.js
+++ dz-form/script.js
@@ -1,10 +1,45 @@
 const formResume = document.forms.resume;
-
 const modal = document.querySelector('.modal');
 const error = document.querySelector('.none');
 const overflow = document.createElement('div');
 overflow.classList.add('overflow');
-
+function validation (arr) {
+    const list = document.createElement('ul');
+    list.classList.add('list');
+    arr.forEach(item => {
+        switch(item.value) {
+            case '':
+                if (item.name === 'name') {
+                    error.classList.add('showError');
+                }
+                item.classList.add('border-red');
+                break;
+            case 'male':
+            case 'female':
+                if( item.checked !== true) {
+                    break;
+                } else {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = item.value;
+                    list.append(listItem);
+                    break;
+                }
+            case 'send':
+                break;
+            default: 
+                const listItem = document.createElement('li');
+                item.classList.remove('border-red');
+                listItem.innerHTML = item.value;
+                list.append(listItem);
+                break;
+        }
+    });
+    if(!Number(formResume.age.value) ) {
+        formResume.age.classList.add('border-red');
+        return;
+    }
+    return list;
+}
 formResume.addEventListener('submit', (e) => {
     e.preventDefault();
     if (document.querySelector('.list')) {
@@ -13,32 +48,11 @@ formResume.addEventListener('submit', (e) => {
     if (error.closest('.showError')) {
         error.classList.remove('showError');
     }
-    const list = document.createElement('ul');
-    list.classList.add('list');
-    for (let item of formResume.elements) {
-        if (item.value === '') {
-            if ( item.name === 'name' ) {
-                error.classList.add('showError');
-            }
-            item.classList.add('border-red');
-        } else if (item.name === 'age' && !Number.isInteger(Number(item.value))) {
-            item.classList.add('border-red');
-        } else if (item.name === 'gender' && item.checked !== true){ 
-            continue;
-        } else if (item.name === 'sub'){ 
-            break;
-        }else {
-            const listItem = document.createElement('li');
-            item.classList.remove('border-red');
-            listItem.innerHTML = item.value;
-            list.append(listItem);
-            
-        }
-    }
+    const arrForm = [...formResume];
+    const list = validation(arrForm);
     if (formResume.name.value === '' || formResume.age.closest('.border-red') || formResume.date.value === '') {
         return;
     }
-    // console.log(formResume.gender.checked);
     modal.style.top = '50%';
     modal.prepend(list);
     document.body.append(overflow);
